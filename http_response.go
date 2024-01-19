b5e71ed4aa574ed1e5b2e30b510159cfac55dc06
Koen de Wolf <koendewolfmail@gmail.com>
2024-01-19 12:37:53 +0100

    Always save bytes

--- http_response.go
+++ http_response.go
@@ -3,53 +3,27 @@ package httpclient
 import (
 	"encoding/json"
 	"encoding/xml"
-	"io"
 	"net/http"
 	"strings"
 )
 
 type HttpResponse struct {
-	readerIsRead bool
-	readCloser   io.ReadCloser
-	body         string
+	body []byte
 
 	StatusCode int64
 	Headers    map[string]string
 }
 
-func (hr *HttpResponse) readBodyAsBytes(saveAsString bool) []byte {
-	if hr.readerIsRead {
-		return []byte(hr.body)
-	}
-
-	bytes := []byte{}
-	_, _ = hr.readCloser.Read(bytes)
-
-	if saveAsString && !hr.readerIsRead {
-		hr.body = string(bytes)
-	}
-
-	hr.readerIsRead = true
-
-	return bytes
+func (hr *HttpResponse) String() string {
+	return string(hr.body)
 }
 
-func (hr *HttpResponse) String(saveAsString bool) string {
-	_ = hr.readBodyAsBytes(saveAsString)
-
-	return hr.body
-}
-
-func (hr *HttpResponse) Json(sliceOrMapOrStruct *any, saveAsString bool) error {
-	bytes := hr.readBodyAsBytes(saveAsString)
-
-	return json.Unmarshal(bytes, sliceOrMapOrStruct)
+func (hr *HttpResponse) Json(sliceOrMapOrStruct *any) error {
+	return json.Unmarshal(hr.body, sliceOrMapOrStruct)
 }
 
 func (hr *HttpResponse) Xml(sliceOrMapOrStruct *any, saveAsString bool) error {
-	bytes := hr.readBodyAsBytes(saveAsString)
-
-	return xml.Unmarshal(bytes, sliceOrMapOrStruct)
+	return xml.Unmarshal(hr.body, sliceOrMapOrStruct)
 }
 
 func newHttpResponse(resp *http.Response) *HttpResponse {
@@ -65,7 +39,9 @@ func newHttpResponse(resp *http.Response) *HttpResponse {
 		hr.Headers[hK] = strings.Join(hV, ", ")
 	}
 
-	hr.readCloser = resp.Body
+	bytes := []byte{}
+	_, _ = resp.Body.Read(bytes)
+	hr.body = bytes
 
 	return hr
 }
