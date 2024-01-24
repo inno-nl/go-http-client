9bc9b3f4378f840c2f07b906f09e8df79d9d44a6
Koen de Wolf <koendewolfmail@gmail.com>
2024-01-24 14:21:40 +0100

    Update http_response.go

--- http_response.go
+++ http_response.go
@@ -39,7 +39,10 @@ func (hr *HttpResponse) readBody() {
 	}
 
 	bytes := []byte{}
-	_, _ = hr.Response.Body.Read(bytes)
+	_, err := hr.Response.Body.Read(bytes)
+	if err != nil {
+		panic(err)
+	}
 	hr.Body = bytes
 }
 
