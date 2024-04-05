6a4bd5f068ce182126d54a79302c7613b43f32d1
Mischa Poslawsky <mischa@inno.nl>
2024-04-05 15:46:30 +0200

    refactor: move shorthand methods to setters include

--- request.go
+++ request.go
@@ -1,11 +1,7 @@
 package httpclient
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -37,13 +33,6 @@ func New() (r *Request) {
 	}
 	r.Parameters = make(url.Values, 0)
 	return
-}
-
-func (r *Request) SetURL(ref string) {
-	r.Request.URL, _ = url.Parse(ref) // final errors reported by Client.Do()
-	if r.Request.URL != nil && r.Request.URL.RawQuery != "" {
-		r.Parameters, _ = url.ParseQuery(r.Request.URL.RawQuery) // TODO error
-	}
 }
 
 func NewURL(ref string) (r *Request) {
@@ -81,60 +70,6 @@ func (r *Request) Clone() *Request {
 	}
 	// Response intentionally kept
 	return d
-}
-
-func (r *Request) Timeout(s int) {
-	r.Client.Timeout = time.Duration(s) * time.Second
-}
-
-func (r *Request) ProxyUrl(ref string) {
-	u, err := url.Parse(ref)
-	if err != nil {
-		r.Error = err
-		return
-	}
-	r.Client.Transport = &http.Transport{Proxy: http.ProxyURL(u)}
-}
-
-func (r *Request) BearerAuth(token string) {
-	r.Request.Header.Set("Authorization", "Bearer "+token)
-}
-
-func (r *Request) BasicAuth(user, pass string) {
-	token := base64.StdEncoding.EncodeToString([]byte(user + ":" + pass))
-	r.Request.Header.Set("Authorization", "Basic "+token)
-}
-
-func (r *Request) Post(body any) {
-	if r.Method == "" {
-		r.Method = "POST"
-	}
-
-	var data []byte
-	switch body.(type) {
-	case nil:
-	case string:
-		data = []byte(body.(string)) // fallthrough
-	case []byte:
-		data = body.([]byte)
-		if _, typeset := r.Request.Header["content-type"]; !typeset {
-			r.Request.Header.Set("Content-Type", "text/plain")
-		}
-	default:
-		var err error
-		data, err = json.Marshal(body)
-		if err != nil {
-			r.Error = err // TODO join
-			return
-		}
-		if _, typeset := r.Request.Header["content-type"]; !typeset {
-			r.Request.Header.Set("Content-Type", "application/json")
-		}
-	}
-	rc := bytes.NewReader(data)
-	r.Request.Body = io.NopCloser(rc)
-	r.Request.ContentLength = int64(rc.Len())
-	// TODO r.Request.GetBody
 }
 
 func (r *Request) Prepare() {
---
+++ setters.go
@@ -0,0 +1,72 @@
+package httpclient
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+func (r *Request) SetURL(ref string) {
+	r.Request.URL, _ = url.Parse(ref) // final errors reported by Client.Do()
+	if r.Request.URL != nil && r.Request.URL.RawQuery != "" {
+		r.Parameters, _ = url.ParseQuery(r.Request.URL.RawQuery) // TODO error
+	}
+}
+
+func (r *Request) Timeout(s int) {
+	r.Client.Timeout = time.Duration(s) * time.Second
+}
+
+func (r *Request) ProxyUrl(ref string) {
+	u, err := url.Parse(ref)
+	if err != nil {
+		r.Error = err
+		return
+	}
+	r.Client.Transport = &http.Transport{Proxy: http.ProxyURL(u)}
+}
+
+func (r *Request) BearerAuth(token string) {
+	r.Request.Header.Set("Authorization", "Bearer "+token)
+}
+
+func (r *Request) BasicAuth(user, pass string) {
+	token := base64.StdEncoding.EncodeToString([]byte(user + ":" + pass))
+	r.Request.Header.Set("Authorization", "Basic "+token)
+}
+
+func (r *Request) Post(body any) {
+	if r.Method == "" {
+		r.Method = "POST"
+	}
+
+	var data []byte
+	switch body.(type) {
+	case nil:
+	case string:
+		data = []byte(body.(string)) // fallthrough
+	case []byte:
+		data = body.([]byte)
+		if _, typeset := r.Request.Header["content-type"]; !typeset {
+			r.Request.Header.Set("Content-Type", "text/plain")
+		}
+	default:
+		var err error
+		data, err = json.Marshal(body)
+		if err != nil {
+			r.Error = err // TODO join
+			return
+		}
+		if _, typeset := r.Request.Header["content-type"]; !typeset {
+			r.Request.Header.Set("Content-Type", "application/json")
+		}
+	}
+	rc := bytes.NewReader(data)
+	r.Request.Body = io.NopCloser(rc)
+	r.Request.ContentLength = int64(rc.Len())
+	// TODO r.Request.GetBody
+}
